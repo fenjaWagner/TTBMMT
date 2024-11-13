2ff75dc1768b1b06f78fda0a292b13aa9101e139
Fenja Wagner <fenja.wagner@uni-jena.de>
2024-11-13 10:34:09 +0100

    implemented single_trace without transposing

--- single_trace.py
+++ single_trace.py
@@ -3,77 +3,29 @@ import numpy as np
 import re
 import mm 
 
-def find_double_indices(term):
-    dim_set = set()
-    single_indices = {}
-    double_indices = {}
 
+def create_dict(term):
+    term_dict = {}
     for i in range(len(term)):
-        dim = term[i]
-        if dim in dim_set:
-            if dim in single_indices:
-                i_list = single_indices.pop(dim)
-                double_indices[dim] = i_list
-            double_indices[dim].append(i)
-        else:
-            dim_set.add(dim)
-            single_indices[dim] = [i]
+        if term[i] not in term_dict:
+            term_dict[term[i]] = [i]
+        else: 
+            term_dict[term[i]].append(i)
+    return term_dict
 
-    return single_indices, double_indices
-
-
-def add_lists(i_dict):
-    new_list = []
-    for k in i_dict:
-        new_list += i_dict[k]
-    return new_list
-
-def add_terms(term_dict, factor = 0):
+def generate_new_term(term_dict, sizes):
     new_term = ""
-    for k in term_dict:
-        if factor == 0:
-            new_term += str(k)
-        else:
-            new_term += str(k)* len(term_dict[k])
-    return new_term
-
-def generate_new_shape(new_term, sizes):
     new_shape = []
-    for i in new_term:
-        new_shape.append(sizes[i])
-    return new_shape
+    for k in term_dict:
+        new_term += str(k)
+        new_shape.append(sizes[k])
+    return new_term, new_shape
 
-        
-
-def create_transpose_tuple(single_indices, double_indices, sizes):
-    single_transpose = add_lists(single_indices)
-    double_transpose = add_lists(double_indices)
-    single_term = add_terms(single_indices)
-    double_term = add_terms(double_indices)
-    double_term_complete = add_terms(double_indices, factor = 1)
-
-    new_term_short = single_term + double_term
-    new_term_complete = single_term + double_term_complete
-
-    transpose_list = tuple(single_transpose + double_transpose)
-    
-    return transpose_list, new_term_short, new_term_complete
-
-
-
-def create_iterator(new_shape):#term_dict, shape):
-    """Creates an iterator that iterates over all the different indices in the term. 
-
-    Args:
-        new_shape (tuple): New shape of the tensor. 
-
-    Returns:
-        iterator: Iterator
-    """
-    iterator = np.ndindex(new_shape)
+def create_iterator(shape):
+    iterator = np.ndindex(tuple(shape))
     return iterator
 
-def calculate_shape_prod(shape):
+def sum_shape(shape):
     shape_sum = [0,]*len(shape)
     prod = 1
     for i in range(len(shape)-1, -1, -1):
@@ -81,60 +33,42 @@ def calculate_shape_prod(shape):
         prod *= shape[i]
     return shape_sum
 
-
-def calc_single_trace(term: str, A: np.array, sizes: dict):
-      
-    single_indices, double_indices = find_double_indices(term)
-    transpose_tuple, new_term_short, new_term_complete = create_transpose_tuple(single_indices, double_indices, sizes)
-    
-    print("single:", single_indices)
-    print("double:", double_indices)
-    if single_indices:
-        print("if")
-        joint_indices = single_indices.copy()
-        joint_indices.update(double_indices)
-        print("joint: ", joint_indices)
-    else: 
-        joint_indices = double_indices
-    
-
-    new_shape_short = generate_new_shape(new_term_short, sizes)
-    iterator = create_iterator(tuple(new_shape_short))
-    new_shape_complete = generate_new_shape(new_term_complete, sizes)
-
-    A = np.ascontiguousarray(np.transpose(A, transpose_tuple))
-    A = np.ascontiguousarray(A.reshape(-1))
-    
-    shape_sum_short = calculate_shape_prod(new_shape_short)
-    shape_sum_complete = calculate_shape_prod(new_shape_complete)
-
+def calc_new_length(shape):
     prod = 1
-    for i in new_shape_short:
+    for i in shape:
         prod *= i
+    return prod
+
+def single_trace(term, A, sizes):
+    term_dict = create_dict(term)
+    new_term, new_shape = generate_new_term(term_dict, sizes)
+    iterator = create_iterator(new_shape)
+    sum_old_shape = sum_shape(A.shape)
+    sum_new_shape = sum_shape(new_shape)
+
+    prod = calc_new_length(new_shape)
     A_new = np.zeros(prod)
     A = A.reshape(-1)
 
-    for index in iterator:   
+    for index in iterator:
         pos_A_new = 0
-        pos_A = 0         
+        pos_A_old = 0
         for i in range(len(index)):
-            pos_A_new += index[i]*shape_sum_short[i]
-            variable = new_term_short[i]
-            for l in joint_indices[variable]:
-                pos_A += index[i]*shape_sum_complete[l]
+            pos_A_new += index[i]*sum_new_shape[i]
+            for j in term_dict[new_term[i]]:
+                pos_A_old += index[i] * sum_old_shape[j]
+            A_new[pos_A_new] = A[pos_A_old]
             
-        A_new[pos_A_new] += A[pos_A]
-    
-    A_new = np.ascontiguousarray(A_new.reshape(new_shape_short))
-    return A_new, new_term_short 
+    A_new = A_new.reshape(tuple(new_shape))
+    return A_new, new_term
 
 
 
 def test_trace():
-    A = np.random.rand(5,5,4,3,5)
+    A = np.random.rand(5,5,4,3,5,3)
     #print(A)
     #A = np.array([[1,2,3], [3,4,5], [5,6,7]])
-    string = "iikji"
+    string = "iikjij"
 
     sizes = {}
     for i in range(len(string)):
@@ -142,11 +76,13 @@ def test_trace():
 
     At = torch.from_numpy(A)
     #B = A.reshape(-1)
-    C, index = calc_single_trace(string, A, sizes)
-    D = torch.einsum("iikji->"+index, At)
+    C, new_term = single_trace(string, A, sizes)
+    print("new_term: ", new_term)
+    D = torch.einsum("iikjij->"+new_term, At)
     
     Ct = torch.from_numpy(C)
     
     print((Ct-D).sum())
 
 test_trace()
+
