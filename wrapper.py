af08d50fa1bd8fd28441c98979c5f6bcc746c682
Fenja Wagner <fenja.wagner@uni-jena.de>
2024-12-30 16:42:27 +0100

    renamed libbmm.so to bmm_libmm.so, added variable datatype to wrapper

--- wrapper.py
+++ wrapper.py
@@ -2,7 +2,7 @@ import numpy as np
 import ctypes
 
 # Load the shared library
-libbmm = ctypes.CDLL('./libbmm.so')  # Use './bmm.dll' on Windows
+libbmm = ctypes.CDLL('./bmm_libbmm.so')  # Use './bmm.dll' on Windows
 
 # Define the TacoTensor structure
 class TacoTensor(ctypes.Structure):
@@ -22,7 +22,7 @@ libbmm.compute.argtypes = [
 libbmm.compute.restype = ctypes.c_int
 
 # Helper function to wrap taco_tensor_t
-def call_cpp_bmm(A: np.array, B: np.array) -> np.array:
+def call_cpp_bmm(A: np.array, B: np.array, data_t: int) -> np.array:
     # Prepare data
     shape_A = np.array(A.shape, dtype=np.int64)
     shape_B = np.array(B.shape, dtype=np.int64)
@@ -40,21 +40,21 @@ def call_cpp_bmm(A: np.array, B: np.array) -> np.array:
         order=3,
         dimensions=shape_A.ctypes.data_as(ctypes.POINTER(ctypes.c_int64)),
         vals=A.ctypes.data_as(ctypes.c_void_p),
-        data_type=0  # DOUBLE = 0
+        data_type = data_t  # DOUBLE = 0
     )
 
     tensor_B = TacoTensor(
         order=3,
         dimensions=shape_B.ctypes.data_as(ctypes.POINTER(ctypes.c_int64)),
         vals=B.ctypes.data_as(ctypes.c_void_p),
-        data_type=0  # DOUBLE = 0
+        data_type= data_t  # DOUBLE = 0
     )
 
     tensor_C = TacoTensor(
         order=3,
         dimensions=shape_C.ctypes.data_as(ctypes.POINTER(ctypes.c_int64)),
         vals=C.ctypes.data_as(ctypes.c_void_p),
-        data_type=0  # DOUBLE = 0
+        data_type= data_t # DOUBLE = 0
     )
 
     # Call the compute function
