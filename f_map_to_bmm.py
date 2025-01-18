def843323dc60e15e3394478c188d5e58db47dd3
Fenja Wagner <fenja.wagner@uni-jena.de>
2025-01-18 16:58:50 +0100

    bugfixing

--- f_map_to_bmm.py
+++ f_map_to_bmm.py
@@ -6,7 +6,7 @@ import math
 import time
 
 
-def map_to_bmm(term_1, term_2, Tensor_1, Tensor_2, contract, batch, keep_1, keep_2, sizes):#full_term_dict, sizes):
+def map_to_bmm(term_1, term_2, Tensor_1, Tensor_2, contract_idcs, batch_idcs, keep_1_idcs, keep_2_idcs, sizes):#full_term_dict, sizes):
     """Maps the contraction of two given tensors to the bmm.
 
     Args:
@@ -20,32 +20,27 @@ def map_to_bmm(term_1, term_2, Tensor_1, Tensor_2, contract, batch, keep_1, keep
         _type_: _description_
     """
 
-    batch_idcs = ''.join(batch)
+    """batch_idcs = ''.join(batch)
     contract_idcs = ''.join(contract)
     keep_1_idcs = ''.join(keep_1)
-    keep_2_idcs = ''.join(keep_2)
-    #print(f"batch {batch_idcs} contract {contract_idcs} keep 1 {keep_1_idcs} keep 2 {keep_2_idcs}")
-    
-    transpose_tuple_1 = tuple(term_1.index(i) for i in batch_idcs+''.join(keep_1)+contract_idcs)
-    print(f"1 term transpose Tensor {term_1, transpose_tuple_1, Tensor_1}")
-    print("Type: ", type(Tensor_1))
-    transpose_tuple_2 = tuple(term_2.index(i) for i in batch_idcs+contract_idcs +''.join(keep_2))
+    keep_2_idcs = ''.join(keep_2)"""
+    result_type = np.result_type(Tensor_1)
+    if np.result_type(Tensor_2) != result_type:
+        Tensor_2.astype(result_type)
 
     size_batch = math.prod([sizes[i] for i in batch_idcs])
     size_sum = math.prod([sizes[i] for i in contract_idcs])
     size_rest_1 = math.prod([sizes[i] for i in keep_1_idcs])
     size_rest_2 = math.prod([sizes[i] for i in keep_2_idcs])
 
-    if transpose_tuple_1:
-        Tensor_1 = np.transpose(Tensor_1, transpose_tuple_1)  
-        print("T1", Tensor_1)  
+    if term_1:
+        Tensor_1 = np.einsum(term_1 +'->'+batch_idcs+keep_1_idcs+contract_idcs, Tensor_1)
+    if term_2:
+        Tensor_2 = np.einsum(term_2 +'->'+batch_idcs+contract_idcs+keep_2_idcs, Tensor_2)
+
     Tensor_1 = np.ascontiguousarray((Tensor_1).reshape((size_batch, size_rest_1, size_sum )))
-    if transpose_tuple_2:
-        Tensor_2 = np.transpose(Tensor_2, transpose_tuple_2)
-        print("T2", Tensor_2)
     Tensor_2 = np.ascontiguousarray((Tensor_2).reshape((size_batch, size_sum, size_rest_2)))
      
-    #O_Tensor = mm.invoke_bmm(Tensor_1, Tensor_2)
     O_Tensor = mm.invoke_c_bmm(Tensor_1, Tensor_2)
     term_O = batch_idcs + keep_1_idcs + keep_2_idcs
 
