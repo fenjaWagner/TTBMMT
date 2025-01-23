91db132a194d36bbac4457fcf31e9c4087363b1b
fenjaWagner <fenja.wagner@posteo.de>
2025-01-23 17:57:59 +0100

    wrote bmm in cpp with template

--- wrapper.py
+++ wrapper.py
@@ -2,7 +2,7 @@ import numpy as np
 import ctypes
 
 # Load the shared library
-libbmm = ctypes.CDLL('./bmm_libbmm.so')  
+libbmm = ctypes.CDLL('./t_libbmm.so')  
 
 # Define the TacoTensor structure
 class TacoTensor(ctypes.Structure):
