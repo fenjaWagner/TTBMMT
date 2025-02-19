9bfc936187b1f3a4e9b54c22cc13387a58e01d81
fenjaWagner <fenja.wagner@posteo.de>
2025-02-19 11:41:11 +0100

    removed unnecessary comments, added time stamps

--- f_map_to_bmm_copy.py
+++ f_map_to_bmm_copy.py
@@ -13,17 +13,16 @@ def prepare_indices(term_A, term_B,  mini_output):
     keep_A = (set_A & output_set) - set_B
     keep_B = (set_B & output_set) - set_A
 
+    # avoid empty tensors
     if not (batch or contract or keep_A):
         keep_A = set_A
-        #flag_A = True
     if not (batch or contract or keep_B):
         keep_B = set_B
-        #flag_B = True
     
     
     return [''.join(set_l) for set_l in [batch, contract, keep_A, keep_B]]
 
-def map_to_bmm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_dict, sizes):
+def map_to_bmm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):
     """Maps the contraction of two given tensors to the bmm.
 
     Args:
@@ -37,16 +36,8 @@ def map_to_bmm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_dic
         _type_: _description_
     """
 
-    """batch_idcs = ''.join(batch)
-    contract_idcs = ''.join(contract)
-    keep_1_idcs = ''.join(keep_1)
-    keep_2_idcs = ''.join(keep_2)"""
-    print(f"Term_1 {term_1}, Term_2 {term_2}, Term_O {term_O}")
     batch_idcs, contract_idcs, keep_1_idcs, keep_2_idcs = prepare_indices(term_1, term_2, term_O)
-    print(f"o_1 {keep_1_idcs}")
-    print(f"o_2 {contract_idcs}")
-    print(f"Shape_1 {Tensor_1.shape}, Shape_2 {Tensor_2.shape}")
-    print(f"sizes {sizes}")
+    
     result_type = np.result_type(Tensor_1)
     if np.result_type(Tensor_2) != result_type:
         Tensor_2.astype(result_type)
@@ -55,7 +46,6 @@ def map_to_bmm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_dic
     size_sum = math.prod([sizes[i] for i in contract_idcs])
     size_rest_1 = math.prod([sizes[i] for i in keep_1_idcs])
     size_rest_2 = math.prod([sizes[i] for i in keep_2_idcs])
-    print(f"batch {size_batch}, contract {size_sum}, rest_1 {size_rest_1}, rest_2 {size_rest_2}")
 
     if term_1:
         Tensor_1 = np.einsum(term_1 +'->'+batch_idcs+keep_1_idcs+contract_idcs, Tensor_1)
@@ -64,23 +54,27 @@ def map_to_bmm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_dic
 
     Tensor_1 = np.ascontiguousarray((Tensor_1).reshape((size_batch, size_rest_1, size_sum )))
     Tensor_2 = np.ascontiguousarray((Tensor_2).reshape((size_batch, size_sum, size_rest_2)))
-     
+
+    tic = time.time()
     O_Tensor = wrapper.call_cpp_bmm(Tensor_1, Tensor_2)
-    term_O_tmp = batch_idcs + keep_1_idcs + keep_2_idcs
+    toc = time.time()
 
-    size_o = tuple([sizes[i] for i in term_O])
+    term_O_tmp = batch_idcs + keep_1_idcs + keep_2_idcs
+    
+
+    size_o = tuple([sizes[i] for i in term_O_tmp])
     
     O_Tensor = np.ascontiguousarray(O_Tensor.reshape(size_o))
     if term_O != term_O_tmp:
         O_Tensor = np.einsum(term_O_tmp+"->"+term_O, O_Tensor)
 
-    return O_Tensor
+    return O_Tensor, toc-tic
 
 
 
 
-def map_to_np_mm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_dict, sizes):
-    """Maps the contraction of two given tensors to the bmm.
+def map_to_np_mm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):
+    """Maps the contraction of two given tensors to the bmm of numpy.
 
     Args:
         term_1 (str): _description_
@@ -93,10 +87,6 @@ def map_to_np_mm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_d
         _type_: _description_
     """
 
-    """batch_idcs = ''.join(batch)
-    contract_idcs = ''.join(contract)
-    keep_1_idcs = ''.join(keep_1)
-    keep_2_idcs = ''.join(keep_2)"""
     batch_idcs, contract_idcs, keep_1_idcs, keep_2_idcs = prepare_indices(term_1, term_2, term_O)
     result_type = np.result_type(Tensor_1)
     if np.result_type(Tensor_2) != result_type:
@@ -115,13 +105,16 @@ def map_to_np_mm(term_1, term_2, term_O, Tensor_1, Tensor_2, sizes):#full_term_d
     Tensor_1 = np.ascontiguousarray((Tensor_1).reshape((size_batch, size_rest_1, size_sum )))
     Tensor_2 = np.ascontiguousarray((Tensor_2).reshape((size_batch, size_sum, size_rest_2)))
      
+    tic = time.time()
     O_Tensor = Tensor_1 @ Tensor_2
+    toc = time.time()
+
     term_O_tmp = batch_idcs + keep_1_idcs + keep_2_idcs
 
-    size_o = tuple([sizes[i] for i in term_O])
+    size_o = tuple([sizes[i] for i in term_O_tmp])
     
     O_Tensor = np.ascontiguousarray(O_Tensor.reshape(size_o))
     if term_O != term_O_tmp:
         O_Tensor = np.einsum(term_O_tmp+"->"+term_O, O_Tensor)
 
-    return O_Tensor
+    return O_Tensor, toc-tic
--- f_path_operation_copy.py
+++ f_path_operation_copy.py
@@ -13,41 +13,48 @@ def build_sizes(term_A, term_B, shape_A, shape_B):
     return sizes
 
 
-def invoke_contraction_torch(A, B, mini_f_string):#, flag_A = False, flag_B = False):
+def invoke_contraction_torch(A, B, mini_f_string):
     A_t = torch.from_numpy(A)
     B_t = torch.from_numpy(B)
+    tic = time.time()
     C = (torch.einsum(mini_f_string, A_t, B_t)).numpy()
-    return C
+    toc = time.time()
+    return C, toc-tic
 
-def invoke_contraction_numpy(A, B, mini_f_string):#, flag_A = False, flag_B = False):
-    C = np.einsum(mini_f_string, A, B)
-    return C
 
-def invoke_contraction_np_mm( A, B,mini_f_string):#, flag_A, flag_B):
+def invoke_contraction_numpy(A, B, mini_f_string):
+    tic = time.time()
+    C = np.einsum(mini_f_string, A, B) 
+    toc = time.time()
+    return C, toc-tic
+    
+def invoke_contraction_np_mm( A, B,mini_f_string):
     mini_inputs, mini_output = mini_f_string.split("->")
     mini_inputs = mini_inputs.split(",")
     term_A = mini_inputs[0]
     term_B = mini_inputs[1]
     sizes = build_sizes(term_A, term_B, A.shape, B.shape)
-    C= f_map_to_bmm_copy.map_to_bmm(term_A, term_B, mini_output, A, B, sizes)
+   
+    C, time_fragment= f_map_to_bmm_copy.map_to_np_mm(term_A, term_B, mini_output, A, B, sizes)
     
     if type(C) != np.ndarray:
         C = np.array(C)
-    return C
+    
+    return C, time_fragment
 
-
-def invoke_contraction_custom( A, B,mini_f_string):#, flag_A, flag_B):
+def invoke_contraction_custom( A, B,mini_f_string):
     mini_inputs, mini_output = mini_f_string.split("->")
     mini_inputs = mini_inputs.split(",")
     term_A = mini_inputs[0]
     term_B = mini_inputs[1]
     sizes = build_sizes(term_A, term_B, A.shape, B.shape)
-    C= f_map_to_bmm_copy.map_to_bmm(term_A, term_B, mini_output, A, B, sizes)
+   
+    C, time_fragment = f_map_to_bmm_copy.map_to_bmm(term_A, term_B, mini_output, A, B, sizes)
     
     if type(C) != np.ndarray:
         C = np.array(C)
-    return C
-
+    
+    return C, time_fragment
 
 
 def prepare_contraction(mini_f_string, A,B, backend = "custom"):
@@ -56,39 +63,30 @@ def prepare_contraction(mini_f_string, A,B, backend = "custom"):
                "numpy": invoke_contraction_numpy,
                "np_mm": invoke_contraction_np_mm}
     method = methods[backend]
-    C = method(A, B,mini_f_string)#, flag_A, flag_B)
-    return C
+    C, time_fragment = method(A, B,mini_f_string)
+    return C, time_fragment
 
-def Cloning(li1):
-    li_copy = li1[:]
-    return li_copy
 
 def work_path(path, tensors_t, format_string, backend = "custom"):
     ssa_path = einsum_benchmark.meta.runtime.to_ssa_path(path)
-    tensors = Cloning(tensors_t)
+    tensors = tensors_t.copy()
     format_string = format_string.replace(" ", "")
     annotated_path = einsum_benchmark.meta.runtime.to_annotated_ssa_path(format_string, ssa_path, True)
     input, output = format_string.split("->")
     tic = time.time()
+    time_fragment = 0
     for t_tuple in annotated_path:
-        print("*"*30)
         first = t_tuple[0]
         second = t_tuple[1]
         mini_f_string = t_tuple[2]
-        print("mini_f_string", mini_f_string)
         A = tensors[first]
         B = tensors[second]
-        C= prepare_contraction(mini_f_string, A, B, backend)
+        C, time_fragment_tmp = prepare_contraction(mini_f_string, A, B, backend)
+        time_fragment += time_fragment_tmp
         tensors.append(C)
-        #term_strings.append(term_C)
-        #print("new_term", term_C)
-
+        
     toc = time.time()
     
-    #if term_C != output:
-    #    C = np.einsum(term_C + "->" + output, C)
-
-    
-    return C, toc-tic
+    return C, toc-tic, time_fragment
 
 
