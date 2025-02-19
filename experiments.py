1e036404337552fcca2fdb044f97a0b41dfb7e47
fenjaWagner <fenja.wagner@posteo.de>
2025-02-19 11:40:36 +0100

    experiments for all different multiplication types

--- experiments.py
+++ experiments.py
@@ -1,37 +1,35 @@
 #import opt_einsum as oe
 import einsum_benchmark
-import f_path_operation as fop
+#import f_path_operation as fop
 import f_path_operation_copy as fo
 import numpy as np
 import ascii
 
 def exp():
-    
-    instance = einsum_benchmark.instances["lm_batch_likelihood_sentence_3_12d"]
-    instance_s = einsum_benchmark.instances["mc_2022_079"]
+    i_list = ["lm_batch_likelihood_sentence_4_8d", "lm_batch_likelihood_sentence_3_12d", "wmc_2023_035", "mc_2021_027", "mc_2022_079"]
 
-    s_opt_size = instance.paths.opt_size
+    for stri in i_list:
+        print(f"*************************************** {stri} *******************************")
 
-    for backend in ["custom"]:
-    #"torch","custom", "numpy", "np_mm"
-    #for backend in ["custom","torch", "numpy", "np_mm"]:
-        print("************************** "+backend+" *********************************")
-        #C, term_C, time = fo.work_path(s_opt_size.path, instance.tensors, instance.format_string, backend)
-        C,term_C, time = fop.work_path(s_opt_size.path, instance.tensors, instance.format_string, backend)
-        print("sum[OUTPUT]:", C.sum(), instance.result_sum)
-        print("time: ", time)
+        instance = einsum_benchmark.instances[stri]
+        s_opt_size = instance.paths.opt_size
+        
+        for backend in ["torch","custom", "numpy", "np_mm"]:
+            C, time, time_fragment = fo.work_path(s_opt_size.path, instance.tensors, instance.format_string, backend)
+            
+            print(f"backend {backend} + time {time} + fragment_time {time_fragment} + difference {time-time_fragment}")
 
-    #"mc_2023_arjun_117" -> 23 s, np 200
-    #"mc_2021_027"0.5, 0.9, 10.7
-    #"mc_2020_082" zu groß
-    #mc_2022_079 0.3, 0.46, 0.21
+        #"mc_2023_arjun_117" -> 23 s, np 200
+        #"mc_2021_027"0.5, 0.9, 10.7
+        #"mc_2020_082" zu groß
+        #mc_2022_079 0.3, 0.46, 0.21
 
-    #"lm_batch_likelihood_sentence_4_8d", 0.8, 27.2, 11.7
-    #lm_batch_likelihood_sentence_3_12d -> 0.2, 4.2, 1,5
-    #mc_rw_blasted_case1_b14_even3 custom: killed
-    # mc_2021_arjun_171 custom: killed
-    # wmc_2021_130 torch: killed
-    # wmc_2023_035 0.9, 1.1, 0.6
+        #"lm_batch_likelihood_sentence_4_8d", 0.8, 27.2, 11.7
+        #lm_batch_likelihood_sentence_3_12d -> 0.2, 4.2, 1,5
+        #mc_rw_blasted_case1_b14_even3 custom: killed
+        # mc_2021_arjun_171 custom: killed
+        # wmc_2021_130 torch: killed
+        # wmc_2023_035 0.9, 1.1, 0.6
 
     
 def blocks():
@@ -41,7 +39,7 @@ def blocks():
     s_opt_size = instance.paths.opt_size
     clock = 0
     for i in range(5):
-        C,term_C, time = fop.work_path(s_opt_size.path, instance.tensors, instance.format_string, "custom")
+        C, time = fo.work_path(s_opt_size.path, instance.tensors, instance.format_string, "custom")
         print(time)
         clock += time
     print("time", clock/5)
@@ -53,7 +51,7 @@ def exp_dtypes():
     C = np.einsum(format_string, A, B)
     for ty in [np.int16, np.int32, np.int64, np.float32, np.float64]:
         print(f"************************** {ty} *************************************" )
-        C_c, term_C, time = fo.work_path([(0,1)], [A,B], format_string, "custom")
+        C_c, time = fo.work_path([(0,1)], [A,B], format_string, "custom")
         print("time: ", time)
         print(np.allclose(C, C_c))
 
@@ -72,4 +70,4 @@ def test_ascii():
     f_list = ascii.convert_ascii_back(f_list_new, char_dict)
     print(f_list)
 
-blocks()
+exp()
