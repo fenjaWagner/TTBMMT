ee73a1fb1c716f060d281b0b9c686cd7b4b59675
fenjaWagner <fenja.wagner@posteo.de>
2025-03-05 19:04:40 +0100

    experiments from the benchmark: runtimes

--- bash_einsum_benchmark.sh
+++ bash_einsum_benchmark.sh
@@ -5,6 +5,6 @@
 for i in $(seq 0 27); do
     for j in $(seq 0 3); do
         echo "Running program with $i and $j"
-        python3 experiments_from_bash.py "$i" "$j"
+        python3 experiments_benchmark_from_bash.py "$i" "$j"
     done
 done
