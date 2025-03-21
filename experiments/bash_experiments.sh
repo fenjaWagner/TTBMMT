b789b1ce9fa7fc95a07f6296519e8793dc1a0a90
fenjaWagner <fenja.wagner@posteo.de>
2025-03-21 18:11:26 +0100

    low number of flops

--- experiments/bash_experiments.sh
+++ experiments/bash_experiments.sh
@@ -12,7 +12,7 @@ done
 
 for backend in $(seq 0 3); do
     for method in $(seq 0 3); do
-        for (( size=5; size<=50; size+=5 )); do
+        for (( size=2; size<=50; size+=5 )); do
             echo "Running program with $backend $method $size"
             python3 experiments_flops.py "$backend" "$method" "$size"
         done
